202b433b966c7c153b138d03c82c3c76485dcd5b
Malksnevicius <michaela.alksnevicius@gmail.com>
2021-05-17 19:06:26 +0200

    Kody

---
+++ for_cycle.py
@@ -0,0 +1,11 @@
+for premenna in range(10):  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print(premenna)
+
+for i in range(2):
+    print(i)
+
+for pismeno in "Ahoj":
+    print(pismeno)
+
+for prvok in ["A", "H", "O", "J", "Svet"]:
+    print(prvok)
