4865dfe267112f5fb03feff8f340a8fb5536d16e
Malksnevicius <michaela.alksnevicius@gmail.com>
2021-05-18 08:44:37 +0200

    Kody

--- cycle_for_leap_year_DU.py
+++ cycle_for_leap_year_DU.py
@@ -5,5 +5,4 @@ for year in range (1950, 1970, 1):
     else:
         print("Rok je neprestupny")
 
-# Tomas, neviem ako do odpovede dostat aj roky,a za tym potom hlasku ci je rok prestupny alebo neprestupny
-# na dnes uz musim skoncit, mozno este zajtra nieco vylepsim
+
