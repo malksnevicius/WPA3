04d2784c97bba5e7a8e6fb37bc501f59b9d2e210
Malksnevicius <michaela.alksnevicius@gmail.com>
2021-05-18 13:22:26 +0200

    Kody

--- cycle_for_leap_year_DU.py
+++ cycle_for_leap_year_DU.py
@@ -1,8 +1,12 @@
-for year in range (1950, 1970, 1):
+year1 = input("Zadaj prvy rok:")
+year2 = input("Zadaj druhy rok:")
+
+year1 = int(year1)
+year2 = int(year2)
+
+for year in range (year1, (year2 + 1)):
     print(year)
     if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
         print("Rok je prestupny")
-    else:
-        print("Rok je neprestupny")
 
 
